32da9d1c259b8033136705c1e734fedecf9c15b7
Soulike <316413310@qq.com>
2019-10-07 15:09:52 +0800

    修改路由，增强通用性

--- src/Dispatcher/Dispatcher.ts
+++ src/Dispatcher/Dispatcher.ts
@@ -1,12 +1,12 @@
 import Router from '@koa/router';
-import {COMMAND, INFO, STATIC} from './ROUTE';
+import {COMMAND, REFS, STATIC} from './ROUTE';
 import {commandService, infoService, staticService} from '../Service';
 import path from 'path';
 import {GIT} from '../CONFIG';
 
 export default (router: Router) =>
 {
-    router.get(INFO, async (ctx) =>
+    router.get(REFS, async (ctx) =>
     {
         const {service} = ctx.request.query;
         if (typeof service !== 'string')
@@ -15,14 +15,14 @@ export default (router: Router) =>
         }
         else
         {
-            const {username, repo} = ctx.params;
-            if (typeof username !== 'string' || typeof repo !== 'string')
+            const {repositoryPath} = ctx.params;
+            if (typeof repositoryPath !== 'string')
             {
                 ctx.response.status = 400;
             }
             else
             {
-                const absoluteRepoPath = path.join(GIT.ROOT, username, repo);
+                const absoluteRepoPath = path.join(GIT.ROOT, repositoryPath);
                 const {statusCode, headers, body} = await infoService(absoluteRepoPath, service);
                 ctx.response.body = body;
                 ctx.response.status = statusCode;
@@ -36,14 +36,14 @@ export default (router: Router) =>
 
     router.post(COMMAND, async (ctx) =>
     {
-        const {username, repo, command} = ctx.params;
-        if (typeof username !== 'string' || typeof repo !== 'string' || typeof command !== 'string')
+        const {repositoryPath, command} = ctx.params;
+        if (typeof repositoryPath !== 'string' || typeof command !== 'string')
         {
             ctx.response.status = 400;
         }
         else
         {
-            const absoluteRepoPath = path.join(GIT.ROOT, username, repo);
+            const absoluteRepoPath = path.join(GIT.ROOT, repositoryPath);
             // 越过 koa 直接操纵请求和响应流
             await commandService(absoluteRepoPath, command, ctx.req, ctx.res);
         }
@@ -51,14 +51,14 @@ export default (router: Router) =>
 
     router.get(STATIC, async (ctx) =>
     {
-        const {username, repo, file} = ctx.params;
-        if (typeof username !== 'string' || typeof repo !== 'string' || typeof file !== 'string')
+        const {repositoryPath} = ctx.params;
+        if (typeof repositoryPath !== 'string')
         {
             ctx.response.status = 400;
         }
         else
         {
-            const absoluteFilePath = path.join(GIT.ROOT, username, repo, file);
+            const absoluteFilePath = path.join(GIT.ROOT, repositoryPath);
             await staticService(absoluteFilePath, ctx.res);
         }
     });
--- src/Dispatcher/ROUTE.ts
+++ src/Dispatcher/ROUTE.ts
@@ -1,4 +1,4 @@
 // git 内置的命令
-export const INFO = '/:username/:repo/info/refs';
-export const COMMAND = '/:username/:repo/git-:command';
-export const STATIC = '/:username/:repo/:file+';
+export const REFS = '/:repositoryPath+/info/refs';
+export const COMMAND = '/:repositoryPath+/git-:command';
+export const STATIC = '/:repositoryPath+';
